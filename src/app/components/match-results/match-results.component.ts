66739c1ac922cde4d2d2e8610b7ca187958dc53b
Chad Wescott <chad.wescott@ayahealthcare.com>
2026-05-19 11:48:19 -0400

    Fixed net score display on player results component to round to 2 decimal places.

--- src/app/components/match-results/match-results.component.ts
+++ src/app/components/match-results/match-results.component.ts
@@ -1,4 +1,4 @@
-import { NgClass } from '@angular/common';
+import { DecimalPipe, NgClass } from '@angular/common';
 import { Component, inject, input } from '@angular/core';
 import { ResultType } from '../../enums/result.enum';
 import { MatchMatchup } from '../../models/match-matchup.model';
@@ -7,7 +7,8 @@ import { AppStateService } from '../../services/app-state.service';
 @Component({
   selector: 'app-match-results',
   imports: [
-    NgClass
+    NgClass,
+    DecimalPipe
   ],
   templateUrl: './match-results.component.html',
   styleUrl: './match-results.component.scss',
